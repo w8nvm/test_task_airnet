568f63c9cfdc2d68fc727aabee160ac7efba2609
Vyacheslav Muravyev <neobuxabuse@gmail.com>
2022-11-27 20:08:50 +0300

    add popup component for adding tasks

--- src/components/CalendarGrid/CalendarGrid.js
+++ src/components/CalendarGrid/CalendarGrid.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import "./CalendarGrid.css"
-import { numOfDays, week } from '../../data/data';
+import { numOfDays, week, months } from '../../data/data';
 import Header from '../Header/Header';
-import { months } from '../../data/data';
+import Popup from '../Popup/Popup';
 
 
 const CalendarGrid = () => {
 
     const [currentDate, setCurrentDate] = useState(new Date());
     const [pickedDate, setPickedDate] = useState(new Date());
-    const [animation, setAnimation] = useState(0);
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
     const firstDayOfTheMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
     const firstDayOfTheView = new Date(firstDayOfTheMonth);
     firstDayOfTheView.setDate(firstDayOfTheView.getDate() - (firstDayOfTheMonth.getDay() || 7) + 1);
-    // console.log(firstDayOfTheMonth);
 
+    const cellBlock = useRef(null);
 
     const days = [...Array(numOfDays)].map((el, ind) => {
         let day = new Date(firstDayOfTheView);
@@ -34,27 +34,20 @@ const CalendarGrid = () => {
         setCurrentDate(new Date(currentDate.setMonth(currentDate.getMonth() + 1)));
     };
 
-    // console.log(days);
-
     return (
         <div
             className='calendarWrapper'
-            onAnimationEnd={() => setAnimation(0)}
-            animation={animation}
         >
             <Header
                 currentDate={currentDate}
                 prevHandler={() => {
                     prevHandler();
-                    setAnimation(0);
                 }}
                 todayHandler={() => {
                     todayHandler();
-                    setAnimation(0);
                 }}
                 nextHandler={() => {
                     nextHandler();
-                    setAnimation(0);
                 }}
             />
             <div className='verticalLineWrapper'>
@@ -72,13 +65,17 @@ const CalendarGrid = () => {
                             i.getFullYear() === new Date().getFullYear();
                         const beforeToday = i < new Date() && !today;
                         const weekend = i.getDay() === 6 || i.getDay() === 0;
-                        const dayWrapperClassname = `dayWrapper`;
+                        const dayWrapperClassname = `dayWrapper ${today ? 'today' : ''}`;
                         const cellWrapperClassname = `cellWrapper ${today ? 'today' : ''}${beforeToday ? 'beforeToday' : ''} ${weekend ? 'weekend' : ''}`;
                         return <div key={i.toString()}
+                            ref={cellBlock}
                             className={cellWrapperClassname}
                             onClick={() => {
                                 setPickedDate(i);
                             }}>
+                            <button className='taskAddingButton' onClick={() => {setIsPopupOpen(true)}} style={{ display: beforeToday ? 'none' : '' }} >
+                                <p className='addButtonText'> {} Add task </p>
+                            </button>
                             <div className='rowInCell'>
                                 <div className={dayWrapperClassname}>
                                     {i.getDate()}
@@ -93,7 +90,7 @@ const CalendarGrid = () => {
                     <h1> {pickedDate?.getDate()} {months[pickedDate?.getMonth()]} </h1>
                 </div>
             </div>
-
+            <Popup isPopupOpen={isPopupOpen} setIsPopupOpen={setIsPopupOpen}/>
         </div>
     )
 }
